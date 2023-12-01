d9dd78534a5fa900ce7557e61aadf29f5331c9bc
Aris7ides <joseamaristam@gmail.com>
2023-12-01 09:58:58 +0100

    ejercicios formularios

--- Und6_Eventos/ejer2.js
+++ Und6_Eventos/ejer2.js
@@ -1,3 +1,15 @@
+// A partir de la pàgina web proporcionada :
+// 1. Quan es mou el ratolí en qualsevol punt de la finestra, s’ha de mostrar la posició
+// del punter respecte al nevagador i respecte de la pàgina en el div info:
+
+// 2. Al polsar qualsevol tecla, el missatge mostrat s’ha de canviar i indicar debe
+// cambiar para indicar el caràcter i el codi ASCII
+
+// 3. Quan es faça clic, el color de fons del div ha de canviar a groc ( #FFFFCC ) i quan
+// es polsa una tecla el color de fons ha de ser blau ( #CCE6FF ). Quan tornem a moure
+// el ratolí, el color de fons torna a ser blanc.
+
+
 let titulo = document.createElement("h1");
 let div = document.getElementById("info");
 let texto=document.createElement("p");
